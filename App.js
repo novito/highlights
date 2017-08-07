5d6e5b486338da1cd741ebce521328264665a712
Joan Roig <roigarderiu@gmail.com>
2017-08-07 14:35:08 -0700

    Fix configuration

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { ListView } from 'react-native';
 import * as firebase from 'firebase';
-import Config from 'react-native-config'
 const HighlightItem = require('./components/HighlightItem');
-const styles = require('./styles.js')
+const styles = require('./styles.js');
+const config = require('./config.json');
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: Config.FIREBASE_API_KEY,
-  authDomain: Config.FIREBASE_AUTH_DOMAIN,
-  databaseURL: Config.FIREBASE_DATABASE_URL,
-  projectId: Config.FIREBASE_STORAGE_BUCKET,
-  storageBucket: Config.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: Config.FIREBASE_MESSAGING_SENDER_ID,
+  apiKey: config.FIREBASE_API_KEY,
+  authDomain: config.FIREBASE_AUTH_DOMAIN,
+  databaseURL: config.FIREBASE_DATABASE_URL,
+  projectId: config.FIREBASE_PROJECT_ID,
+  storageBucket: config.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID,
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
@@ -21,8 +21,31 @@ export default class App extends React.Component {
     super(props);
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      dataSource: ds.cloneWithRows([{ title: "First tooth" }]),
+      dataSource: ds.cloneWithRows([{ title: "test"}, { title: "test" }]),
     };
+    this.highlightsRef = firebaseApp.database().ref();
+  }
+
+  componentDidMount() {
+    this.listenForHighlights(this.highlightsRef);
+  }
+
+  listenForHighlights(highlightsRef) {
+    highlightsRef.on('value', (snap) => {
+
+      // get children as an array
+      var items = [];
+      snap.forEach((child) => {
+        items.push({
+          title: child.val().title,
+          _key: child.key
+        });
+      });
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+    });
   }
 
   _renderHighlightItem(item) {
